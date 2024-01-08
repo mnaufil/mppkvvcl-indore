4b899450dda821e5154ae1b060f915aed9f8136e
mansiparab-benchmark <mansi.p@benchmarksolution.co.in>
2024-01-08 18:50:06 +0530

    Adding mypdf library

--- app/application/libraries/mypdf.php
+++ app/application/libraries/mypdf.php
@@ -2,6 +2,8 @@
 <?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
   
 require_once dirname(__FILE__) . '/fpdf/fpdf.php';
+
+#[\AllowDynamicProperties]
   
 class Mypdf extends FPDF
 {

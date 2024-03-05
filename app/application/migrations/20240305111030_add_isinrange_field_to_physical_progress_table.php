a8b7294ae2d9c5799ccf0715204f40fb6269d64c
mansiparab-benchmark <mansi.p@benchmarksolution.co.in>
2024-03-05 11:32:00 +0530

    Migration Script
    Added field is_inrange to physical_progress table

---
+++ app/application/migrations/20240305111030_add_isinrange_field_to_physical_progress_table.php
@@ -0,0 +1,31 @@
+<?php defined('BASEPATH') OR exit('No direct script access allowed');
+/**
+ * 
+ */
+class Migration_Add_isinrange_field_to_physical_progress_table extends CI_Migration
+{
+	function __construct()
+	{
+		parent::__construct();
+		$this->load->dbforge();
+	}
+
+	public function up()
+	{
+		$fields = array(
+			'is_inrange' => array(
+				'type' => 'BIT',
+				'null' => TRUE,
+				'after' => 'geo_code'
+			)
+		);
+
+		$this->dbforge->add_column('physical_progress', $fields);
+	}
+
+	public function down()
+	{
+		$this->dbforge->drop_column('physical_progress', 'is_inrange');
+	}
+}
+?>
